faa9a036716beaa8db35ba190b62087d852424cb
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2023-12-22 10:23:48 +0000

    wip..

--- Server/Routes/code.ts
+++ Server/Routes/code.ts
@@ -1,21 +1,19 @@
 /*
-type of requests - CREATE/REMOVE/LOGIN/CHECK
-key 
-adminPassword
-ipv4
-    ____                       ___         __  __               __  _ _____            __                
-   / __ \___  ____ _________  /   | __  __/ /_/ /_  ___  ____  / /_(_) __(_)________ _/ /_____  _____    
-  / /_/ / _ \/ __ `/ ___/ _ \/ /| |/ / / / __/ __ \/ _ \/ __ \/ __/ / /_/ / ___/ __ `/ __/ __ \/ ___/    
- / ____/  __/ /_/ / /__/  __/ ___ / /_/ / /_/ / / /  __/ / / / /_/ / __/ / /__/ /_/ / /_/ /_/ / /        
-/_/    \___/\__,_/\___/\___/_/  |_\__,_/\__/_/ /_/\___/_/ /_/\__/_/_/ /_/\___/\__,_/\__/\____/_/                                                    
----------------------------------------------------------*/
-import { Base, BaseGuild, BaseGuildTextChannel, Client, EmbedBuilder } from "discord.js";
+ *     ____                       ___         __  __               __  _ _____            __                
+ *    / __ \___  ____ _________  /   | __  __/ /_/ /_  ___  ____  / /_(_) __(_)________ _/ /_____  _____    
+ *   / /_/ / _ \/ __ `/ ___/ _ \/ /| |/ / / / __/ __ \/ _ \/ __ \/ __/ / /_/ / ___/ __ `/ __/ __ \/ ___/    
+ *  / ____/  __/ /_/ / /__/  __/ ___ / /_/ / /_/ / / /  __/ / / / /_/ / __/ / /__/ /_/ / /_/ /_/ / /        
+ * /_/    \___/\__,_/\___/\___/_/  |_\__,_/\__/_/ /_/\___/_/ /_/\__/_/_/ /_/\___/\__,_/\__/\____/_/                                                    
+*/
+
+import { BaseGuildTextChannel, Client, EmbedBuilder } from "discord.js";
 import { Request, Response } from "express";
 
 import { QuickDB } from 'quick.db';
 import { config } from '../config';
 
 const db = new QuickDB();
+
 interface Code {
     good: {
         title: string
@@ -25,8 +23,8 @@ interface Code {
         title: string
         descriptions: string,
         main?: {
-            ip: any,
-            key: any
+            ip: string,
+            key: string
         }
     },
     bad: {
@@ -62,23 +60,24 @@ const data: Code = {
 export = async (req: Request, res: Response, client: Client) => {
     const { ip, key, tor, adminKey } = req.body;
 
-    if (!ip || !key) return console.log("-> Bad json request without ip/key");
+    if (!ip || !key) {
+        console.log("-> Bad json request without ip/key");
+        return;
+    };
 
-    console.log(tor)
     if (tor !== 'CREATE_KEY' && tor !== 'DELETE_KEY' && tor !== 'CHECK_KEY' && tor !== 'LOGIN_KEY') {
         console.log('-> Bad json requests without options');
-        return res.send('-> Bad json requests without options');
+        res.send('-> Bad json requests without options');
+        return;
     }
 
     console.log(`--------------------------------------------------------------------------------------------------------------------\nIP: ${ip}\nREQUEST TYPE: ${tor}\nADMINKEY: ${adminKey}\nKEY: ${key}\n--------------------------------------------------------------------------------------------------------------------`)
 
-    if (tor == "CREATE_KEY") {
-        console.log(adminKey, config.adminKey)
-        if (adminKey != config.adminKey) return console.log("-> Bad json requests without the good admin key")
+    if (tor == "CREATE_KEY" && adminKey != config.adminKey) {
         await db.set(`key_${key}`, ip);
-        res.send("Succefully create a key !")
+        res.send("Succefully create a key !");
 
-        const embed = new EmbedBuilder()
+        let embed = new EmbedBuilder()
             .setTitle("Server ~:")
             .setColor("#008000")
             .setDescription(`A key has created\`\`\`${key}\`\`\`This IPV4 is **${ip}**`)
@@ -86,16 +85,19 @@ export = async (req: Request, res: Response, client: Client) => {
             .setTimestamp();
 
         (client.channels.cache.get(config.sendID) as BaseGuildTextChannel).send({ embeds: [embed] });
-        return
-    }
+        return;
+    } else {
+        console.log("-> Bad json requests without the good admin key");
+        res.send(500);
+    };
 
-    if (tor == "DELETE_KEY") {
-        if (adminKey != config.adminKey) return console.log("-> Bad json requests without the good admin key")
+    if (tor == "DELETE_KEY" && adminKey != config.adminKey) {
+
         let value = await db.get(`key_${key}.${ip}`);
         if (!value) return;
-        res.send("Succefully delete a key !")
+        res.send("Succefully delete a key !");
 
-        const embed = new EmbedBuilder()
+        let embed = new EmbedBuilder()
             .setTitle("Server ~:")
             .setColor("#008000")
             .setDescription(`A key has deleted\`\`\`${key}\`\`\`This IPV4 is **${value}**`)
@@ -104,19 +106,24 @@ export = async (req: Request, res: Response, client: Client) => {
 
         (client.channels.cache.get(config.sendID) as BaseGuildTextChannel).send({ embeds: [embed] });
         await db.delete(`key_${key}.${ip}`);
-        return
-    }
+        return;
+    } else {
+        console.log("-> Bad json requests without the good admin key")
+    };
 
-    if (tor == "LOGIN_KEY") {
-        const { ip, key, tor, adminKey } = req.body
-        if (!ip || !key) return console.log("-> Bad json request without ip/key")
-        if (tor != 'LOGIN_KEY') return
-        if (adminKey != 'unknow') return
+    if (tor == "LOGIN_KEY" && adminKey == 'unknow') {
+
         let value = await db.get(`key_${key}`);
-        if (!value) { return res.json(data.bad2) };
-        if (value != ip) return res.json(data.bad);
 
-        const embed = new EmbedBuilder()
+        if (!value) {
+            res.json(data.bad2);
+            return;
+        } else if (value != ip) {
+            res.json(data.bad);
+            return;
+        };
+
+        let embed = new EmbedBuilder()
             .setTitle("Server ~:")
             .setColor("#008000")
             .setDescription(`A key has use for login\`\`\`${key}\`\`\`This IPV4 is **${value}**`)
@@ -126,22 +133,20 @@ export = async (req: Request, res: Response, client: Client) => {
         (client.channels.cache.get(config.sendID) as BaseGuildTextChannel).send({ embeds: [embed] });
         res.json(data.good);
         return;
-    }
+    };
 
-    if (tor == "CHECK_KEY") {
-        const { ip, key, tor, adminKey } = req.body
-        if (!ip || !key) return console.log("-> Bad json request without ip/key")
-        if (adminKey != config.adminKey) return
+    if (tor == "CHECK_KEY" && adminKey != config.adminKey) {
+
         let value = await db.get(`key_${key}`);
-        if (!value) { return res.json(data.bad2); }
         let fetchIPV4 = { ip: value, key: key }
 
+        if (!value) {
+            return res.json(data.bad2);
+        };
 
         data.good2.main = fetchIPV4;
         res.json(data.good2)
         return;
-    }
-}
+    };
+};
 //made by Kisakay
-//inspirate is not skid
-//anti-skid
